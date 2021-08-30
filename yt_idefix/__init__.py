9703feb8380cca648d5f785f35256992376e3e3a
Clément Robert <cr52@protonmail.com>
2021-08-30 19:55:57 +0200

    ENH: take advantage of the fact that in yt 4.0, dataset child classes register themselves

--- yt_idefix/__init__.py
+++ yt_idefix/__init__.py
@@ -1,11 +1,5 @@
-# register our dataset classes manually
-from yt.utilities.object_registries import output_type_registry
-
 from .data_structures import IdefixDumpDataset, IdefixGrid, IdefixHierarchy
 from .fields import IdefixFieldInfo
 from .io import IdefixIOHandler
 
-output_type_registry["IdefixDumpDataset"] = IdefixDumpDataset
-
-
-__version__ = "0.1.1"
+__version__ = "0.1.2"
