385d3bc1c947c28d9fcaa828fb7e2c2f60171c03
Clément Robert <cr52@protonmail.com>
2021-09-12 19:26:34 +0200

    ENH: add provisional support for vtk data

--- yt_idefix/fields.py
+++ yt_idefix/fields.py
@@ -2,7 +2,25 @@ from yt.fields.field_info_container import FieldInfoContainer
 from yt.fields.magnetic_field import setup_magnetic_field_aliases
 
 
-class IdefixFieldInfo(FieldInfoContainer):
+class IdefixVtkFieldInfo(FieldInfoContainer):
+    known_other_fields = (  # type: ignore
+        ("RHO", ("code_mass / code_length**3", ["density"], None)),
+        ("VX1", ("code_length / code_time", ["velocity_x"], None)),
+        ("VX2", ("code_length / code_time", ["velocity_y"], None)),
+        ("VX3", ("code_length / code_time", ["velocity_z"], None)),
+        ("BX1", ("code_magnetic", [], None)),
+        ("BX2", ("code_magnetic", [], None)),
+        ("BX3", ("code_magnetic", [], None)),
+        ("PRS", ("code_pressure", ["pressure"], None)),
+    )
+
+    def setup_fluid_fields(self):
+        setup_magnetic_field_aliases(
+            self, "idefix-vtk", [f"BX{idir}" for idir in "123"]
+        )
+
+
+class IdefixDmpFieldInfo(FieldInfoContainer):
     known_other_fields = (  # type: ignore
         ("Vc-RHO", ("code_mass / code_length**3", ["density"], None)),
         ("Vc-VX1", ("code_length / code_time", ["velocity_x"], None)),
@@ -19,4 +37,6 @@ class IdefixFieldInfo(FieldInfoContainer):
     known_particle_fields = ()
 
     def setup_fluid_fields(self):
-        setup_magnetic_field_aliases(self, "idefix", [f"Vc-BX{idir}" for idir in "123"])
+        setup_magnetic_field_aliases(
+            self, "idefix-dmp", [f"Vc-BX{idir}" for idir in "123"]
+        )
--- yt_idefix/io.py
+++ yt_idefix/io.py
@@ -5,38 +5,32 @@ import numpy as np
 
 from yt.utilities.io_handler import BaseIOHandler
 
-from ._io import dmp_io
+from ._io import dmp_io, vtk_io
 
 
 class IdefixIOHandler(BaseIOHandler, ABC):
     _particle_reader = False
-    _dataset_type = "idefix"
+    _dataset_type = "idefix-NONE"
 
     def __init__(self, ds):
         BaseIOHandler.__init__(self, ds)
         self.ds = ds
-        self.dmpfile = ds.parameter_filename
+        self._data_file = ds.parameter_filename
 
     def _read_fluid_selection(self, chunks, selector, fields, size):
         data = {}
         offset = 0
 
-        cls = self.__class__
-        with open(self.dmpfile, mode="rb") as fh:
+        with open(self._data_file, mode="rb") as fh:
             for field in fields:
                 ftype, fname = field
                 data[ftype, fname] = np.empty(size, dtype="float64")
                 for chunk in chunks:
                     for grid in chunk.objs:
                         foffset = grid._index._field_offsets[fname]
-                        values = cls._read_single_field(fh, foffset)
+                        values = self._read_single_field(fh, foffset)
                         offset += grid.select(selector, values, data[field], offset)
         return data
-
-    @staticmethod
-    @abstractmethod
-    def _read_single_field(fh: BinaryIO, offset: int) -> np.ndarray:
-        pass
 
     def _read_particle_coords(self, chunks, ptf):
         # This needs to *yield* a series of tuples of (ptype, (x, y, z)).
@@ -55,12 +49,21 @@ class IdefixIOHandler(BaseIOHandler, ABC):
         # computation.
         raise NotImplementedError
 
+    # The following methods are frontend-specific
+    @abstractmethod
+    def _read_single_field(self, fh: BinaryIO, offset: int) -> np.ndarray:
+        pass
+
 
 class IdefixVtkIOHandler(IdefixIOHandler):
-    ...
+    _dataset_type = "idefix-vtk"
+
+    def _read_single_field(self, fh: BinaryIO, offset: int) -> np.ndarray:
+        return vtk_io.read_single_field(fh, offset, shape=self.ds.domain_dimensions)
 
 
 class IdefixDmpIOHandler(IdefixIOHandler):
-    @staticmethod
-    def _read_single_field(fh: BinaryIO, offset: int) -> np.ndarray:
+    _dataset_type = "idefix-dmp"
+
+    def _read_single_field(self, fh: BinaryIO, offset: int) -> np.ndarray:
         return dmp_io.read_single_field(fh, offset)
