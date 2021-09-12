11f5bfe06054283b205d512edfc5ec3e050c5baa
Clément Robert <cr52@protonmail.com>
2021-09-12 21:25:48 +0200

    FIX: fix broken api module

--- yt_idefix/api.py
+++ yt_idefix/api.py
@@ -1,4 +1,10 @@
-from .data_structures import IdefixDmpDataset, IdefixGrid, IdefixHierarchy
-from .dmpfile_io import read_idefix_dmpfile
+from ._io.dmp_io import read_idefix_dmpfile
+from .data_structures import (
+    IdefixDmpDataset,
+    IdefixDmpHierarchy,
+    IdefixGrid,
+    IdefixVtkDataset,
+    IdefixVtkHierarchy,
+)
 from .fields import IdefixFieldInfo
 from .io import IdefixIOHandler
