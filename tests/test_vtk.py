8cde0f7135b1d259f95c4cb7e159757451759e57
Clément Robert <cr52@protonmail.com>
2021-12-27 18:26:05 +0100

    TST: add tests for geometry parsing in vtk based datasets

--- tests/test_vtk.py
+++ tests/test_vtk.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import Any, Dict
 
 import pytest
 from packaging.version import Version
@@ -9,36 +10,53 @@ import yt
 import yt_idefix
 from yt_idefix.api import IdefixVtkDataset, PlutoVtkDataset
 
+DatabaseT = Dict[str, Dict[str, Any]]
+
 YT_VERSION = Version(yt.__version__)
 
 DATA_DIR = Path(__file__).parent / "data"
-IDEFIX_VTK_FILES = {
+
+IDEFIX_VTK_FILES: DatabaseT = {
     # KHI in cartesian coordinates, geom included in datafile, no streching
     "khi": {
         "path": DATA_DIR.joinpath("khi", "data.0100.vtk"),
-        "geometry": None,
+        "has_geometry": True,
+        "geometry": "cartesian",
+        "dimensionality": 2,
         "kind": "idefix",
     },
     # contributed by Gaylor Wafflard Fernandez; this dataset stretched in z and R
-    # "planet_disk": {"path": DATA_DIR.joinpath("planet-disk", "data.0034.vtk"), "geometry": "polar", "kind": "idefix"},
+    "planet_disk": {
+        "path": DATA_DIR.joinpath("planet-disk", "data.0034.vtk"),
+        "has_geometry": False,
+        "geometry": "polar",
+        "dimensionality": 3,
+        "kind": "idefix",
+    },
     # contributed by Marc Van den Bossche; this dataset is stretched in the radial direction
     "cataclysmic_disk": {
         "path": DATA_DIR.joinpath("cataclysmic-disk", "data.0086.vtk"),
+        "has_geometry": False,
         "geometry": "polar",
+        "dimensionality": 2,
         "kind": "idefix",
     },
     # small spherical 3D test case
     "spherical_disk": {
         "path": DATA_DIR.joinpath("FargoMHDSpherical", "data.0010.vtk"),
+        "has_geometry": True,
         "geometry": "spherical",
+        "dimensionality": 3,
         "kind": "idefix",
     },
 }
 
-PLUTO_VTK_FILES = {
+PLUTO_VTK_FILES: DatabaseT = {
     "sedov": {
         "path": DATA_DIR.joinpath("pluto_sedov", "data.0018.vtk"),
+        "has_geometry": True,
         "geometry": "cartesian",
+        "dimensionality": 3,
         "kind": "pluto",
         "units": {
             "length": unyt_quantity(3.08567758e21, "cm"),
@@ -49,14 +67,26 @@ PLUTO_VTK_FILES = {
         },
     },
     "sod": {
+        "has_geometry": True,
         "path": DATA_DIR.joinpath("pluto_sod", "data.0001.vtk"),
         "geometry": "cartesian",
+        "dimensionality": 1,
         "kind": "pluto",
         "current_time": None,
     },
 }
 
 VTK_FILES = {**IDEFIX_VTK_FILES, **PLUTO_VTK_FILES}
+
+# subset of files in which geometry isn't parsable and needs to be provided by the user
+VTK_FILES_NO_GEOMETRY = {
+    k: v for k, v in VTK_FILES.items() if v["has_geometry"] is False
+}
+
+# and the complementary
+VTK_FILES_WITH_GEOMETRY = {
+    k: v for k, v in VTK_FILES.items() if v["has_geometry"] is True
+}
 
 
 @pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
@@ -129,12 +159,32 @@ def test_data_access(file):
 
 
 @pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
-def test_load(file):
+def test_load_user_passed_geometry(file):
     ds = yt_idefix.load(file["path"], geometry=file["geometry"])
-    if file["geometry"] is None:
-        assert ds.geometry in ("cartesian", "polar", "spherical")
-    else:
-        assert ds.geometry == file["geometry"]
+    assert ds.geometry == file["geometry"]
+    assert ds.dimensionality == file["dimensionality"]
+
+
+@pytest.mark.parametrize(
+    "file", VTK_FILES_NO_GEOMETRY.values(), ids=VTK_FILES_NO_GEOMETRY.keys()
+)
+def test_load_missing_geometry_metadata(file):
+    with pytest.raises(
+        ValueError,
+        match=(
+            "Geometry couldn't be parsed from file. "
+            "The 'geometry' keyword argument must be specified."
+        ),
+    ):
+        yt_idefix.load(file["path"])
+
+
+@pytest.mark.parametrize(
+    "file", VTK_FILES_WITH_GEOMETRY.values(), ids=VTK_FILES_WITH_GEOMETRY.keys()
+)
+def test_load_parseable_geometry_metadata(file):
+    ds = yt_idefix.load(file["path"])
+    assert ds.geometry == file["geometry"]
 
 
 # TODO: make this a pytest-mpl test
