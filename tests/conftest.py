d47b481d043e586e835a8ef3d5bf3a8ee3cca37a
Clément Robert <cr52@protonmail.com>
2021-12-27 20:24:11 +0100

    RFC: refactor testing database infrastructure to enable testing again large datasets locally without adding them to version control

---
+++ tests/conftest.py
@@ -0,0 +1,127 @@
+from __future__ import annotations
+
+import os
+import re
+from pathlib import Path
+from typing import Any
+
+import pytest
+import unyt as un
+import yaml
+
+
+def parse_quantity(s) -> un.unyt_quantity:
+    # FUTURE: use unyt.unyt_quantity.from_string instead
+    # https://github.com/yt-project/unyt/pull/191
+
+    # This is partially adapted from the following SO thread
+    # https://stackoverflow.com/questions/41668588/regex-to-match-scientific-notation
+    _NUMB_PATTERN = r"^[+/-]?((?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)|\d*\.?\d+|\d+\.?\d*|nan\s|inf\s)"  # noqa: E501
+    # *all* greek letters are considered valid unit string elements.
+    # This may be an overshoot. We rely on unyt.Unit to do the actual validation
+    _UNIT_PATTERN = r"([α-ωΑ-Ωa-zA-Z]+(\*\*([+/-]?[0-9]+)|[*/])?)+"
+    _QUAN_PATTERN = fr"{_NUMB_PATTERN}\s*{_UNIT_PATTERN}"
+    _NUMB_REGEXP = re.compile(_NUMB_PATTERN)
+    _UNIT_REGEXP = re.compile(_UNIT_PATTERN)
+    _QUAN_REGEXP = re.compile(_QUAN_PATTERN)
+
+    v = s.strip()
+    match = re.fullmatch(_NUMB_PATTERN, v)
+    if match is not None:
+        return float(match.group()) * un.Unit()
+    if not re.match(_QUAN_REGEXP, v):
+        raise ValueError(f"Received invalid quantity expression '{s}'.")
+    res = re.search(_NUMB_REGEXP, v)
+    if res is None:
+        raise ValueError
+    num = res.group()
+    res = re.search(_UNIT_REGEXP, v[res.span()[1] :])
+    if res is None:
+        raise ValueError
+    unit = res.group()
+    return float(num) * un.Unit(unit)
+
+
+DATA_DIR = Path(__file__).parent / "data"
+
+VTK_FILES: dict[str, dict[str, Any]] = {}
+
+
+def load_meta(pdir, meta_file):
+    with open(meta_file) as fh:
+        metadata = yaml.load(fh, yaml.SafeLoader)
+
+    metadata["attrs"]["path"] = pdir / metadata["attrs"]["path"]
+    if "units" in metadata["attrs"]:
+        keys = list(metadata["attrs"]["units"].keys())
+        for u in keys:
+            metadata["attrs"]["units"][u] = parse_quantity(
+                metadata["attrs"]["units"][u]
+            )
+    return metadata
+
+
+for ddir in os.listdir(DATA_DIR):
+    pdir = DATA_DIR / ddir
+    meta_file = pdir / "meta.yaml"
+    if not pdir.is_dir():
+        continue
+    if not meta_file.is_file():
+        continue
+
+    metadata = load_meta(pdir, meta_file)
+    VTK_FILES.update({metadata["id"]: metadata["attrs"]})
+
+
+@pytest.fixture(params=VTK_FILES.values(), ids=VTK_FILES.keys(), scope="session")
+def vtk_file(request):
+    return request.param
+
+
+# useful subsets
+VTK_FILES_NO_GEOMETRY = {
+    k: v for k, v in VTK_FILES.items() if v["has_geometry"] is False
+}
+
+
+@pytest.fixture(
+    params=VTK_FILES_NO_GEOMETRY.values(),
+    ids=VTK_FILES_NO_GEOMETRY.keys(),
+    scope="session",
+)
+def vtk_file_no_geom(request):
+    return request.param
+
+
+VTK_FILES_WITH_GEOMETRY = {
+    k: v for k, v in VTK_FILES.items() if v["has_geometry"] is True
+}
+
+
+@pytest.fixture(
+    params=VTK_FILES_WITH_GEOMETRY.values(),
+    ids=VTK_FILES_WITH_GEOMETRY.keys(),
+    scope="session",
+)
+def vtk_file_with_geom(request):
+    return request.param
+
+
+IDEFIX_VTK_FILES = {k: v for k, v in VTK_FILES.items() if v["kind"] == "idefix"}
+
+
+@pytest.fixture(
+    params=IDEFIX_VTK_FILES.values(), ids=IDEFIX_VTK_FILES.keys(), scope="session"
+)
+def idefix_vtk_file(request):
+    return request.param
+
+
+PLUTO_VTK_FILES = {k: v for k, v in VTK_FILES.items() if v["kind"] == "pluto"}
+
+
+@pytest.fixture(
+    params=PLUTO_VTK_FILES.values(), ids=PLUTO_VTK_FILES.keys(), scope="session"
+)
+def pluto_vtk_file(request):
+    return request.param
--- tests/test_vtk.py
+++ tests/test_vtk.py
@@ -1,96 +1,18 @@
 import os
-from pathlib import Path
-from typing import Any, Dict
 
 import pytest
 from packaging.version import Version
-from unyt import assert_allclose_units, unyt_quantity
+from unyt import assert_allclose_units
 
 import yt
 import yt_idefix
 from yt_idefix.api import IdefixVtkDataset, PlutoVtkDataset
 
-DatabaseT = Dict[str, Dict[str, Any]]
-
 YT_VERSION = Version(yt.__version__)
 
-DATA_DIR = Path(__file__).parent / "data"
 
-IDEFIX_VTK_FILES: DatabaseT = {
-    # KHI in cartesian coordinates, geom included in datafile, no streching
-    "khi": {
-        "path": DATA_DIR.joinpath("khi", "data.0100.vtk"),
-        "has_geometry": True,
-        "geometry": "cartesian",
-        "dimensionality": 2,
-        "kind": "idefix",
-    },
-    # contributed by Gaylor Wafflard Fernandez; this dataset stretched in z and R
-    "planet_disk": {
-        "path": DATA_DIR.joinpath("planet-disk", "data.0034.vtk"),
-        "has_geometry": False,
-        "geometry": "polar",
-        "dimensionality": 3,
-        "kind": "idefix",
-    },
-    # contributed by Marc Van den Bossche; this dataset is stretched in the radial direction
-    "cataclysmic_disk": {
-        "path": DATA_DIR.joinpath("cataclysmic-disk", "data.0086.vtk"),
-        "has_geometry": False,
-        "geometry": "polar",
-        "dimensionality": 2,
-        "kind": "idefix",
-    },
-    # small spherical 3D test case
-    "spherical_disk": {
-        "path": DATA_DIR.joinpath("FargoMHDSpherical", "data.0010.vtk"),
-        "has_geometry": True,
-        "geometry": "spherical",
-        "dimensionality": 3,
-        "kind": "idefix",
-    },
-}
-
-PLUTO_VTK_FILES: DatabaseT = {
-    "sedov": {
-        "path": DATA_DIR.joinpath("pluto_sedov", "data.0018.vtk"),
-        "has_geometry": True,
-        "geometry": "cartesian",
-        "dimensionality": 3,
-        "kind": "pluto",
-        "units": {
-            "length": unyt_quantity(3.08567758e21, "cm"),
-            "velocity": unyt_quantity(1e5, "cm/s"),
-            "mass": unyt_quantity(4.91416082e40, "g"),
-            "time": unyt_quantity(3.08567758e16, "s"),
-            "magnetic": unyt_quantity(4.58462477e-07, "G"),
-        },
-    },
-    "sod": {
-        "has_geometry": True,
-        "path": DATA_DIR.joinpath("pluto_sod", "data.0001.vtk"),
-        "geometry": "cartesian",
-        "dimensionality": 1,
-        "kind": "pluto",
-        "current_time": None,
-    },
-}
-
-VTK_FILES = {**IDEFIX_VTK_FILES, **PLUTO_VTK_FILES}
-
-# subset of files in which geometry isn't parsable and needs to be provided by the user
-VTK_FILES_NO_GEOMETRY = {
-    k: v for k, v in VTK_FILES.items() if v["has_geometry"] is False
-}
-
-# and the complementary
-VTK_FILES_WITH_GEOMETRY = {
-    k: v for k, v in VTK_FILES.items() if v["has_geometry"] is True
-}
-
-
-@pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
-def test_class_validation(file):
+def test_class_validation(vtk_file):
+    file = vtk_file
     cls = {
         "idefix": IdefixVtkDataset,
         "pluto": PlutoVtkDataset,
@@ -98,8 +20,8 @@ def test_class_validation(file):
     assert cls._is_valid(file["path"])
 
 
-@pytest.mark.parametrize("file", PLUTO_VTK_FILES.values(), ids=PLUTO_VTK_FILES.keys())
-def test_parse_pluto_metadata(file):
+def test_parse_pluto_metadata(pluto_vtk_file):
+    file = pluto_vtk_file
     ds = yt_idefix.load(file["path"])
     assert os.path.samefile(
         ds._definitions_header, file["path"].parent / "definitions.h"
@@ -114,8 +36,8 @@ def test_parse_pluto_metadata(file):
         assert ds.current_time >= 0
 
 
-@pytest.mark.parametrize("file", PLUTO_VTK_FILES.values(), ids=PLUTO_VTK_FILES.keys())
-def test_pluto_units(file):
+def test_pluto_units(pluto_vtk_file):
+    file = pluto_vtk_file
     ds = yt_idefix.load(file["path"])
     if "units" not in file:
         return
@@ -123,7 +45,7 @@ def test_pluto_units(file):
         assert_allclose_units(getattr(ds, f"{u}_unit"), expected)
 
 
-def test_pluto_wrong_definitions_header():
+def test_pluto_wrong_definitions_header(pluto_vtk_file):
     with pytest.raises(
         FileNotFoundError,
         match=(
@@ -131,12 +53,10 @@ def test_pluto_wrong_definitions_header():
             "The 'geometry' keyword argument must be specified."
         ),
     ):
-        yt_idefix.load(
-            PLUTO_VTK_FILES["sedov"]["path"], definitions_header="definitions2.h"
-        )
+        yt_idefix.load(pluto_vtk_file["path"], definitions_header="definitions2.h")
 
 
-def test_pluto_wrong_definitions_header_with_geometry():
+def test_pluto_wrong_definitions_header_with_geometry(pluto_vtk_file):
     with pytest.warns(
         UserWarning,
         match=(
@@ -145,30 +65,27 @@ def test_pluto_wrong_definitions_header_with_geometry():
         ),
     ):
         yt_idefix.load(
-            PLUTO_VTK_FILES["sedov"]["path"],
+            pluto_vtk_file["path"],
             definitions_header="definitions2.h",
             geometry="cartesian",
         )
 
 
-@pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
-def test_data_access(file):
+def test_data_access(vtk_file):
+    file = vtk_file
     ds = yt_idefix.load(file["path"], geometry=file["geometry"])
     ad = ds.all_data()
     ad["gas", "density"]
 
 
-@pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
-def test_load_user_passed_geometry(file):
+def test_load_user_passed_geometry(vtk_file):
+    file = vtk_file
     ds = yt_idefix.load(file["path"], geometry=file["geometry"])
     assert ds.geometry == file["geometry"]
     assert ds.dimensionality == file["dimensionality"]
 
 
-@pytest.mark.parametrize(
-    "file", VTK_FILES_NO_GEOMETRY.values(), ids=VTK_FILES_NO_GEOMETRY.keys()
-)
-def test_load_missing_geometry_metadata(file):
+def test_load_missing_geometry_metadata(vtk_file_no_geom):
     with pytest.raises(
         ValueError,
         match=(
@@ -176,20 +93,18 @@ def test_load_missing_geometry_metadata(file):
             "The 'geometry' keyword argument must be specified."
         ),
     ):
-        yt_idefix.load(file["path"])
+        yt_idefix.load(vtk_file_no_geom["path"])
 
 
-@pytest.mark.parametrize(
-    "file", VTK_FILES_WITH_GEOMETRY.values(), ids=VTK_FILES_WITH_GEOMETRY.keys()
-)
-def test_load_parseable_geometry_metadata(file):
+def test_load_parseable_geometry_metadata(vtk_file_with_geom):
+    file = vtk_file_with_geom
     ds = yt_idefix.load(file["path"])
     assert ds.geometry == file["geometry"]
 
 
 # TODO: make this a pytest-mpl test
-@pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
-def test_slice_plot(file):
+def test_slice_plot(vtk_file):
+    file = vtk_file
     ds = yt_idefix.load(file["path"], geometry=file["geometry"], unit_system="code")
     if YT_VERSION <= Version("4.0.1"):
         if ds.geometry == "spherical":
@@ -208,12 +123,10 @@ def test_slice_plot(file):
     reason="all .vtk files are detected as Athena-compatible, "
     "hence dataformat is considered ambiguous before yt 4.0.2",
 )
-@pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
-def test_load_magic(file):
-    ds = yt.load(file["path"], geometry=file["geometry"])
+def test_load_magic(vtk_file):
+    ds = yt.load(vtk_file["path"], geometry=vtk_file["geometry"])
     assert isinstance(ds, IdefixVtkDataset)
 
 
-@pytest.mark.parametrize("file", VTK_FILES.values(), ids=VTK_FILES.keys())
-def test_load_stretched(file):
-    yt_idefix.load_stretched(file["path"], geometry=file["geometry"])
+def test_load_stretched(vtk_file):
+    yt_idefix.load_stretched(vtk_file["path"], geometry=vtk_file["geometry"])
