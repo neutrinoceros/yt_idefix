b04be1429594f9b184cb6b2cfadc2b27f0adeb84
Clément Robert <cr52@protonmail.com>
2023-01-28 17:22:16 +0100

    TYP: don't ignore all missing imports in typechecking

--- yt_idefix/_typing.py
+++ yt_idefix/_typing.py
@@ -2,7 +2,7 @@
 # so we don't want to use `from __future__ import annotation` here as long as yt doesn't
 from typing import Tuple, Union
 
-from unyt import unyt_quantity
+from unyt import unyt_quantity  # type: ignore [import]
 
 # an intentionally restrictive list of types that can
 # be passes to ds.quan (which is a proxy for unyt.unyt_quantity.__init__)
